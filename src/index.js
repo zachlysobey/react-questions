046a7e594283e188385cd6bff8b1529fa8aab02e
zach lysobey <zach@lysobey.com>
2016-02-24 21:36:01 -0500

    add a secondsElapsed timer to the app

--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@
 const root = document.getElementById('app');
 
 const App = React.createClass({
+  getInitialState: function() {
+    return { secondsElapsed: 0 };
+  },
+  tick: function() {
+    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+  },
+  componentDidMount: function() {
+    this.interval = setInterval(this.tick, 1000);
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
   render: function() {
-    return React.createElement('p', null, 'hello ' + this.props.name);
+    return React.createElement('p', null, 'hello ' + this.props.name + ' ' + this.state.secondsElapsed);
   }
 });
 
