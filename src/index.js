f7bca5a0854446a3120979ab74d12042b801c6db
zach lysobey <zach@lysobey.com>
2016-02-24 21:24:20 -0500

    create react class and pass name prop

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 const root = document.getElementById('app');
-const elem = React.createElement('p', null, 'hello world');
+
+const App = React.createClass({
+  render: function() {
+    return React.createElement('p', null, 'hello ' + this.props.name);
+  }
+});
+
+
+const elem = React.createElement(App, {name: 'React'});
 ReactDOM.render(elem, root);
 
