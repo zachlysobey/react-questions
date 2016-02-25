4bffe218e821b990ceea1e13d06f984b37850901
zach lysobey <zach@lysobey.com>
2016-02-24 20:42:31 -0500

    chore: create webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+var path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: []
+    }
+};
+
