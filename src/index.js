fd10978dfce38386c1b170f759c9361c63cd95e1
zach lysobey <zach@lysobey.com>
2016-03-03 23:25:54 -0500

    convert index.js to index.jsx

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app.jsx';
-
-const root = document.getElementById('app');
-const elem = React.createElement(App, {name: 'React'});
-
-ReactDOM.render(elem, root);
