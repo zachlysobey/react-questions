64783aa1a9aa1f4bd3886d9b23b7876c52fe5907
zach lysobey <zach@lysobey.com>
2016-03-03 23:19:55 -0500

    dynamically create <SecondCounter>s

--- src/app.jsx
+++ src/app.jsx
@@ -3,12 +3,19 @@ import SecondCounter from './second-counter.jsx';
 
 export default React.createClass({
   render: function() {
+    const intervals = [1, 2, 3, 4, 5];
+    const counters = intervals.map(interval => (
+      <p key={interval}>
+        {interval} Second Counter:
+        {' '}
+        <SecondCounter secondInterval={interval} />
+      </p>
+    ));
+    console.log(counters);
     return (
       <div>
         <h1>React Playground</h1>
-        <p>1 Second Counter: <SecondCounter secondInterval="1" /></p>
-        <p>2 Second Counter: <SecondCounter secondInterval="2" /></p>
-        <p>5 Second Counter: <SecondCounter secondInterval="5" /></p>
+        {counters}
       </div>
     );
   }
