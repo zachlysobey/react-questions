fc9f0af88381740004051f931627031457998f7f
zach lysobey <zach@lysobey.com>
2016-03-06 10:33:50 -0500

    echo stuff out to help debug deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-set -e 
+set -e
 
 npm run clean-build
 
-cd build 
+cd build
 
 git init
 git config user.name "Travis CI"
 git config user.email "zlysobey@gmail.com"
 git add .
 git commit -m "Deploy to GitHub Pages"
+
+echo "https://${GH_TOKEN}@${GH_REF}"
 
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
